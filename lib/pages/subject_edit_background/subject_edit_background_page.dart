9ae1bb9ac876569d27468430e7b64034699afec7
Maxiee <maxieewong@gmail.com>
2025-01-05 21:27:04 +0800

    Add background initialization and save functionality in SubjectEditBackgroundPage: integrate background loading and saving with SubjectHomeProvider

--- lib/pages/subject_edit_background/subject_edit_background_page.dart
+++ lib/pages/subject_edit_background/subject_edit_background_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
+import 'package:home_mentor/pages/subject_home/subject_home_provider.dart';
+import 'package:provider/provider.dart';
 
 class SubjectEditBackgroundPage extends StatefulWidget {
   const SubjectEditBackgroundPage({super.key});
@@ -11,6 +15,14 @@ class SubjectEditBackgroundPage extends StatefulWidget {
 
 class _SubjectEditBackgroundPageState extends State<SubjectEditBackgroundPage> {
   final _controller = QuillController.basic();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.document = Document.fromJson(jsonDecode(
+        Provider.of<SubjectHomeProvider>(context, listen: false)
+            .currentSubject?['background']));
+  }
 
   @override
   void dispose() {
@@ -40,6 +52,14 @@ class _SubjectEditBackgroundPageState extends State<SubjectEditBackgroundPage> {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Save the background
+            Provider.of<SubjectHomeProvider>(context, listen: false)
+                .updateBackground(
+                    jsonEncode(_controller.document.toDelta().toJson()));
+          },
+          child: const Icon(Icons.save)),
     );
   }
 }
